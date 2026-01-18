9573619a46891f2a4ee58f37ae13256f6226a03f
SachinthaDaham <dahamsachintha66@gmail.com>
2026-01-18 11:00:00 +0530

    Implemented backend API communication layer for ML model Flask integration

---
+++ lib/http_client/http_client.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioClient {
+
+  static final DioClient _instance = DioClient._internal();
+  late Dio dio;
+
+  factory DioClient() {
+    return _instance;
+  }
+
+  DioClient._internal() {
+    BaseOptions options = BaseOptions(
+      baseUrl: '',
+      connectTimeout: Duration(milliseconds: 10000),
+      receiveTimeout: Duration(milliseconds: 10000),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+
+    dio = Dio(options);
+    dio.interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+      maxWidth: 90,
+    ));
+  }
+}

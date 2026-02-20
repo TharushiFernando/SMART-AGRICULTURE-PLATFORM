7697bc609dbfe9ad7641420182d329f13e146ffc
SachinthaDaham <dahamsachintha66@gmail.com>
2026-02-20 10:15:00 +0530

    Map Gradient Boosting health predictions to UI categories

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,7 @@ const String FLASK_API_BASE = 'http://api.smartcoconut.com';
 // Fertilizer thresholds
 const double N_MIN = 2.0;
 const double P_MIN = 3.5;
+// Health Categories
+const String CATEGORY_CRITICAL = 'Critical';
+const String CATEGORY_MODERATE = 'Moderate';
+const String CATEGORY_HEALTHY = 'Healthy';
